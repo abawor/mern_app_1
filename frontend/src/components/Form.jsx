b2769c38ba1aeee5a1ac3dca1bec6b0227a538ad
abawor <abawor200@gmail.com>
2025-01-31 15:25:57 +0000

    Added form data console logging

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,12 +1,47 @@
+
 function Form() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const logData = {
+            name: formData.get("name"),
+            age: formData.get("age")
+        }
+        console.log(logData)
+    }
+
     return (
         <div className="m-10">
             <div className="text-2xl">Learning MERN App Dev</div>
-            <div className="flex flex-col mt-10">
-                <input type="text" id="name" placeholder="Enter your name" className="mb-5 border-1 w-50 p-1"/>
-                <input type="number" id="age" placeholder="Enter your age" className="mb-5 border-1 w-50 p-1"/>
-                <button className="bg-green-400 mb-5 border-1 w-50 p-1">Submit</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="flex flex-col mt-10">
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Enter your name"
+                        required
+                        className="mb-5 border-1 w-50 p-1"
+                    />
+                    <input
+                        type="number"
+                        id="age"
+                        name="age"
+                        placeholder="Enter your age"
+                        required
+                        className="mb-5 border-1 w-50 p-1"
+                    />                
+                    <button
+                        className="mb-5 border-1 w-50 p-1 bg-green-400 hover:bg-green-500 active:bg-green-700"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }
--- frontend/src/components/LogsDisplay.jsx
+++ frontend/src/components/LogsDisplay.jsx
@@ -5,7 +5,7 @@ function LogsDisplay() {
         <div className="m-10"> 
             {logs.map(log => (
                 <div key={log.id} className="mt-5 border-l-2 w-30 p-1">
-                    <div>{log.name}</div>
+                    <div>{log.name.charAt(0).toUpperCase() + log.name.slice(1)}</div>
                     <div>{log.age}</div>
                 </div>
             ))}
