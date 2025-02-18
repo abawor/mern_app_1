92f51c2e813f87d4be8a3e268d5f7e69d9fa18df
abawor200 <abawor200@gmail.com>
2025-02-18 18:11:17 +0000

    Now updating logs with live input data

--- frontend/src/components/LogsDisplay.jsx
+++ frontend/src/components/LogsDisplay.jsx
@@ -1,9 +1,52 @@
 import { useEffect, useState } from "react";
 import { MdDeleteForever } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
+import { FaRegSave } from "react-icons/fa";
 
 function LogsDisplay() {
     const [logs, setLogs] = useState([]);
+    const [editing, setEditing] = useState()
+    const [activeEditing, setActiveEditing] = useState(false)
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+    
+    
+    const enterEditMode = (log) => {
+        if (activeEditing) return
+        setActiveEditing(true)
+        setName(log.name)
+        setAge(log.age)
+        setEditing({...editing, [log._id]: "True"})
+    }
+    
+    const handleEditLog = async (logId, name, age) => {
+        
+        const log = {
+            _id: logId,
+            name: name,
+            age: age
+        }
+
+        try {
+            const response = await fetch("http://localhost:3000/log", {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(log)
+            })
+            if (response.ok) {
+                window.location.reload()
+            } else {
+                console.error("Error updating log")
+            }
+        } catch (error) {
+            console.error("Error updating log:", error)
+        } finally {
+            setEditing({...editing, _id: "False"})
+            setActiveEditing(false)
+        }
+    }
 
     const handleDeleteLog = async (logId) => {
 
@@ -33,32 +76,6 @@ function LogsDisplay() {
         }
     }
 
-    const handleEditLog = async (logId, name, age) => {
-
-        const log = {
-            _id: logId,
-            name: name,
-            age: age
-        }
-
-        try {
-            const response = await fetch("http://localhost:3000/log", {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(log)
-            })
-            if (response.ok) {
-                window.location.reload()
-            } else {
-                console.error("Error updating log")
-            }
-        } catch (error) {
-            console.error("Error updating log:", error)
-        }
-    }
-
     useEffect(() => {
         fetch("http://localhost:3000/logs")
         .then(res => res.json())
@@ -67,18 +84,42 @@ function LogsDisplay() {
         .catch(err => console.error("Error fetching data: ", err))
     }, [])
 
+    useEffect(() => {
+        setEditing(Object.fromEntries(logs.map(log => [log._id, "False"])))
+    }, [logs])
+
     return (
         <div className="m-10"> 
             {logs.map(log => (
                 <div key={log._id} className="mt-5 border-l-2 w-30 p-1">
-                    <div className="font-medium">{log.name.charAt(0).toUpperCase() + log.name.slice(1)}</div>
-                    <div>{log.age}</div>
+                    <div
+                        className={`font-medium mb-1 + ${editing[log._id] === "True" ? "rounded-sm border-1 border-solid" : ""}`}
+                        suppressContentEditableWarning={true}
+                        contentEditable={editing[log._id]}
+                        onInput={e => setName(e.currentTarget.textContent)}
+                    >
+                        {log.name.charAt(0).toUpperCase() + log.name.slice(1)}
+                    </div>
+                    <div
+                        className={editing[log._id] === "True" ? "rounded-sm border-1 border-solid" : ""}
+                        suppressContentEditableWarning={true}
+                        contentEditable={editing[log._id]}
+                        onInput={e => setAge(e.currentTarget.textContent)}
+                    >
+                        {log.age}
+                    </div>
                     <div className="flex flex-row mt-2">
-                        <CiEdit
-                            size={20}
-                            className="cursor-pointer mr-2"
-                            onClick={() => handleEditLog(log._id, "updated", "99")}
-                        />
+                        {editing[log._id] === "True" ? 
+                            <FaRegSave
+                                size={20}
+                                className="cursor-pointer mr-2"
+                                onClick={() => handleEditLog(log._id, name, age)}
+                            /> :
+                            <CiEdit
+                                size={20}
+                                className="cursor-pointer mr-2"
+                                onClick={() => enterEditMode(log)}
+                        />}
                         <MdDeleteForever 
                             size={19}
                             className="cursor-pointer"
